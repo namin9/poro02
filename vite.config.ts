75fa95c59b933eb740dca97653aee2d62edab985
namin9 <96509320+namin9@users.noreply.github.com>
2025-10-01 15:58:33 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
-// Cloudflare Pages는 정적 자산을 배포하고, /functions 아래가 BFF로 동작
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   server: { port: 5173 }
 })
